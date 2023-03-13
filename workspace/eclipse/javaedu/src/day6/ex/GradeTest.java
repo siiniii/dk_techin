d94069c4422b634b1054df437e4737cb7c1ffc51
siiniii <seungini97@gmail.com>
2023-03-14 00:03:22 +0900

    ex

--- workspace/eclipse/javaedu/src/day6/ex/GradeTest.java
+++ workspace/eclipse/javaedu/src/day6/ex/GradeTest.java
@@ -2,59 +2,82 @@ package day6.ex;
 
 import java.util.Scanner;
 
-class GradeExpr{
+class GradeExpr {
 	int[] jumsu;
-	
-	GradeExpr(int[] jumsu){
+	int sum = 0;
+
+	GradeExpr(int jumsu[]) {
 		this.jumsu = jumsu;
+
 	}
-	
-	double getAverage(double avg) {
-		avg = (jumsu[0] + jumsu[jumsu.length - 1]) / jumsu.length;
-		return avg;
+
+	double getAverage() {
+		for (int i = 0; i < jumsu.length; i++) {
+			sum += jumsu[i];
+		}
+		return sum / jumsu.length;
 	}
-	
-	int getTotal(int sum) {
-		sum = jumsu[0] + jumsu[jumsu.length - 1];
+
+	int getTotal() {
+		for (int i = 0; i < jumsu.length; i++) {
+			sum += jumsu[i];
+		}
 		return sum;
 	}
-	
-	int getGoodScore(int goodScore) {
+
+	int getGoodScore() {
 		int good = jumsu[0];
 		for (int i = 1; i < jumsu.length; i++) {
 			if (good < jumsu[i]) {
 				good = jumsu[i];
 			}
 		}
-		return goodScore;
+		return good;
 	}
-	
-	int getBadScore(int badScore) {
+
+	int getBadScore() {
 		int bad = jumsu[0];
 		for (int i = 1; i < jumsu.length; i++) {
 			if (bad > jumsu[i]) {
 				bad = jumsu[i];
 			}
 		}
-		return badScore;
+		return bad;
 	}
-	
+
 }
 
 public class GradeTest {
 
 	public static void main(String[] args) {
+		System.out.print("점수를 입력하세요 : ");
+
 		Scanner sc = new Scanner(System.in);
-		System.out.printf("데이터를 입력하세요 : ");
-		
 		int[] arr = new int[sc.nextInt()];
 		sc.nextLine();
-		GradeExpr ge = new GradeExpr(arr);
-		
-		
-		String line = sc.nextLine();
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+			if (i == 0) {
+				System.out.print("점수들 : ");
+			} else {
+				System.out.print(arr[i]);
+			}
+			if (i < arr.length - 1) {
+				System.out.print(",");
+			}
 			
-		System.out.printf("점수들 : %d", arr);
+		}
+		
+		System.out.println();
+		GradeExpr ge = new GradeExpr(arr);
+
+		System.out.println("총점 : " + ge.getTotal());
+		System.out.println("평균 : " + ge.getAverage());
+		System.out.println("최고 점수 : " + ge.getGoodScore());
+		System.out.println("최저 점수 : " + ge.getBadScore());
+		
+		sc.close();
 	}
 
 }
