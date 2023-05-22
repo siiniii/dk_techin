80d578db167a0bac24bb30aef92adc4640fc2142
siiniii <seungini97@gmail.com>
2023-05-22 14:44:49 +0900

    0522_ECMAScript

--- workspace/IntelliJ/springedu2/src/main/java/springrest/exam/controller/BoardController.java
+++ workspace/IntelliJ/springedu2/src/main/java/springrest/exam/controller/BoardController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import springrest.exam.domain.Board;
 @Slf4j
 @RestController
 @RequestMapping("/boards")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 public class BoardController {
 
 	List<Board> boardList = new ArrayList<>();
